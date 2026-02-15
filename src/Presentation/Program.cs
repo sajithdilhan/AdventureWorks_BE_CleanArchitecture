fe1b833ccbcfa434c8edd129c1ff9afee19b4843
Sajith Dilhan <sajithdilhan@gmail.com>
2026-02-15 11:46:09 +0800

    Initial Setup

---
+++ src/Presentation/Program.cs
@@ -0,0 +1,30 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference(options =>
+    {
+        options.WithTitle("Adventure Works API")
+               .WithTheme(ScalarTheme.Default)
+               .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient)
+               .EnableDarkMode();
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
